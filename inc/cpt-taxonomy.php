e5811c76c1672da71e12507cf03e760692051994
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-05-20 16:47:41 -0700

    registered staff cpt and taxonomy

---
+++ inc/cpt-taxonomy.php
@@ -0,0 +1,74 @@
+<?php
+// Register staff cpt
+function school_register_custom_post_types(){
+    $labels = array(
+        'name'               => _x( 'Staff', 'post type general name'  ),
+        'singular_name'      => _x( 'Staff', 'post type singular name'  ),
+        'menu_name'          => _x( 'Staff', 'admin menu'  ),
+        'name_admin_bar'     => _x( 'Staff', 'add new on admin bar' ),
+        'add_new'            => _x( 'Add New', 'service' ),
+        'add_new_item'       => __( 'Add New Staff' ),
+        'new_item'           => __( 'New  Staff'),
+        'edit_item'          => __( 'Edit Staff' ),
+        'view_item'          => __( 'View Staff'  ),
+        'all_items'          => __( 'All Staff' ),
+        'search_items'       => __( 'Search Staff' ),
+        'parent_item_colon'  => __( 'Parent Staff:' ),
+        'not_found'          => __( 'No services found.' ),
+        'not_found_in_trash' => __( 'No services found in Trash.' ),
+    );
+    
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'staff' ),
+        'capability_type'    => 'post',
+        'has_archive'        => false,
+        'hierarchical'       => false,
+        'menu_position'      => 7,
+        'menu_icon'          => 'dashicons-businessperson',
+        'supports'           => array( 'title' )
+    );
+    
+    register_post_type( 'school-staff', $args );
+}
+add_action( 'init', 'school_register_custom_post_types' );
+
+// Register taxonomies
+function school_register_taxonomies(){
+    // Add staff taxonomy
+
+    $labels = array(
+        'name'              => _x( 'Staff Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Staff Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Staff Categories' ),
+        'all_items'         => __( 'All Staff Category' ),
+        'parent_item'       => __( 'Parent Staff Category' ),
+        'parent_item_colon' => __( 'Parent Staff Category:' ),
+        'edit_item'         => __( 'Edit Staff Category' ),
+        'view_item'         => __( 'Vview Staff Category' ),
+        'update_item'       => __( 'Update Staff Category' ),
+        'add_new_item'      => __( 'Add New Staff Category' ),
+        'new_item_name'     => __( 'New Staff Category Name' ),
+        'menu_name'         => __( 'Staff Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'staff-categories' ),
+    );
+    register_taxonomy( 'school-staff-category', array( 'school-staff' ), $args );
+}
+add_action( 'init', 'school_register_taxonomies');
+

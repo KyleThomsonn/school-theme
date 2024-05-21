b4630a180077326159846a1bf2bb9abd59792605
Yasin Colak <yasincolak35@gmail.com>
2024-05-21 16:23:06 -0700

    Added Student Taxonomy

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -110,6 +110,36 @@ function school_register_taxonomies(){
         'rewrite'           => array( 'slug' => 'staff-categories' ),
     );
     register_taxonomy( 'school-staff-category', array( 'school-staff' ), $args );
+
+    //Add Student Taxonomy
+
+    $labels = array(
+        'name'              => _x( 'Student Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Student Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Student Categories' ),
+        'all_items'         => __( 'All Student Category' ),
+        'parent_item'       => __( 'Parent Student Category' ),
+        'parent_item_colon' => __( 'Parent Student Category:' ),
+        'edit_item'         => __( 'Edit Student Category' ),
+        'view_item'         => __( 'Vview Student Category' ),
+        'update_item'       => __( 'Update Student Category' ),
+        'add_new_item'      => __( 'Add New Student Category' ),
+        'new_item_name'     => __( 'New Student Category Name' ),
+        'menu_name'         => __( 'Student Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'student-categories' ),
+    );
+    register_taxonomy( 'school-student-category', array( 'school-student' ), $args );
+
 }
 add_action( 'init', 'school_register_taxonomies');
 

5c0a07a1d707c5fa531836956046c42a948497cd
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-05-21 17:01:46 -0700

    made change in student cpt

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -67,7 +67,7 @@ function school_register_custom_post_types(){
         'query_var'          => true,
         'rewrite'            => array( 'slug' => 'students' ),
         'capability_type'    => 'post',
-        'has_archive'        => false,
+        'has_archive'        => true,
         'hierarchical'       => false,
         'menu_position'      => 7,
         'menu_icon'          => 'dashicons-smiley',

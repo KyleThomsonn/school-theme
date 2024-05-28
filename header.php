0c523ba14d97b8fde9596066ea0939ec0e51d778
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-05-27 21:56:39 -0700

    started styles for header

--- header.php
+++ header.php
@@ -45,7 +45,10 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'school-theme' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+  			<path fill="none" d="M0 0h24v24H0z"/>
+  			<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+			</svg></button>
 			<?php
 			wp_nav_menu(
 				array(
